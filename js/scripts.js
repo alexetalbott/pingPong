5d6bb18c708358abb0e28ec6b2d9121b70331e9b
Alex Talbott <alex.e.talbott@gmail.com>
2018-03-11 18:03:03 -0500

    add working solution for counting up to a number that still needs styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 //business logic
 
+//var Rule1 = function(input){
+    //return input;
+//  };
+
 
 //user interface logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var pingPongInput = parseInt($"input#yourNumber").val();
-    var result = pingPongFunction (pingPongInput);
-    
+    var pingPongInput = parseInt($("input#yourNumber").val());
+    var counting = [];
+    for (var index = 0; index < pingPongInput; index += 1) {
+      counting.push(index);
+    };
+    $(".showNumber").text(counting);
+    $("#results").show();
   })
-}
+});
+
+// create a blank array
+// assign userInput to a variable
+// create a loop that pushes numbers AND displays it into the blank array for as long as index < userInput
+// once that works, create ANOTHER function that replaces the numbers?
