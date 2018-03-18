35c6241a6ddddff3620d1a65fd15814f18d41f93
Alex Talbott <alex.e.talbott@gmail.com>
2018-03-18 14:38:15 -0500

    updated scripts.js to separate business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
+//business logic
+var magic = function(pingPongInput) {
+  var counting = [];
+  for (var index = 0; index <= pingPongInput; index += 1) {
+    if ((index % 3 === 0) && (index % 5 === 0)) {
+      counting.push("PingPong");
+    } else if (index % 3 === 0){
+    counting.push("ping");
+    } else if (index % 5 === 0){
+    counting.push("pong");
+    } else {
+    counting.push(index);
+    };
+  };
+  return counting.join("\n");
+};
 
-
+//user interface logic
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     var pingPongInput = parseInt($("input#yourNumber").val());
-    var counting = [];
-    for (var index = 0; index <= pingPongInput; index += 1) {
-      if ((index % 3 === 0) && (index % 5 === 0)) {
-        counting.push("PingPong");
-      } else if (index % 3 === 0){
-      counting.push("ping");
-      } else if (index % 5 === 0){
-      counting.push("pong");
-      } else {
-      counting.push(index);
-      };
-    };
-  $(".showNumber").text(counting.join("\n"));
+    var result = magic(pingPongInput);
+  $(".showNumber").text(result);
   $("#results").show();
 });
 });
